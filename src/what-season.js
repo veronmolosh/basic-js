8458af0322ec7386788bbd1742ab3e3bb6568760
veronmolosh <nikabu17@gmail.com>
2021-03-26 17:20:29 +0300

    refactor: add a condition for throwing an error

--- src/what-season.js
+++ src/what-season.js
@@ -4,15 +4,15 @@ module.exports = function getSeason(date) {
     if (date === undefined) {
         return 'Unable to determine the time of year!';
     }
-    if (isNaN(Date.parse(date))) {
+    if (isNaN(Date.parse(date)) || typeof(+date) !== 'number') {
         throw new Error();
     }
     let month = date.getMonth();
     if (month === 11 || month <= 1) {
         return 'winter';
-    } else if (month <= 4) {
+    } else if (month >= 2 && month <= 4) {
         return 'spring';
-    } else if (month <= 7) {
+    } else if (month >= 5 && month <= 7) {
         return 'summer';
     } else {
         return 'autumn';
