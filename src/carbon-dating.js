b21eac1377e92ee4487e017d613bcb3217b71b9f
veronmolosh <nikabu17@gmail.com>
2021-03-27 15:27:53 +0300

    feat: add a check condition false

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+    if (sampleActivity == undefined || sampleActivity !== 'number') {
+        return false;
+    }
 };
