c3a22b05a1f2db3e5c4d200473f8a4b7866abfa6
veronmolosh <nikabu17@gmail.com>
2021-03-30 12:51:01 +0300

    feat: add solution task carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,11 +2,13 @@ const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
+let result;
 
 module.exports = function dateSample(sampleActivity) {
-    if (sampleActivity == undefined || sampleActivity !== 'number' || isNaN(sampleActivity)) {
+    if (sampleActivity == undefined || typeof sampleActivity !== 'string' || isNaN(sampleActivity) || sampleActivity >= 15 || sampleActivity <= 0) {
         return false;
     } else {
-
+        result = Math.log(MODERN_ACTIVITY / sampleActivity) * HALF_LIFE_PERIOD / 0.693;
     }
+    return Math.ceil(result);
 };
